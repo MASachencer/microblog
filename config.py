2ac2f96d78fd8cdd0bd8566f6f3db53f910d9496
Matt <ms_achencer@yahoo.com>
2018-07-20 11:15:39 +0800

    databases

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql+pymysql://matt:mojiezuo1991@localhost/microblog'
--- microblog.py
+++ microblog.py
@@ -2,3 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from app import app
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
