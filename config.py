d5832efb0725bd96a06c914c41eed90337755868
Matt <ms_achencer@yahoo.com>
2018-07-23 14:30:56 +0800

    pagination

--- config.py
+++ config.py
@@ -18,3 +18,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['ms_achencer@wo.cn']
+    POSTS_PER_PAGE = 25
